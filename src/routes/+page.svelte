340422e1b726921611f371b6605e71c699ce8a91
Anand Muley <anandbmuley@gmail.com>
2025-04-12 22:10:16 +0530

    Revise introduction and features section for clarity and conciseness in JSON Viewer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,25 +4,27 @@
 
 <div data-bs-theme="dark" class="container page-center">
 	<h1 class="display-1">JSON Viewer</h1>
-	<h2>Introduction</h2>
-	<p>JSON viewer is helpful tool for working with JSON strings. Below are some of its features:</p>
+	<h2>Features</h2>
+	<p>JSON Viewer simplifies working with JSON data. Key features include:</p>
 	<ul>
-		<li>
-			<b>Tree View</b>
-			<p>You can see a tree view of the JSON content in a user readable format.</p>
-		</li>
-		<li>
-			<b>Saving</b>
-			<p>You can save the JSON content by giving a title and refer it later.</p>
-		</li>
+		<li><b>Tree View:</b> Visualize JSON in an easy-to-read tree structure.</li>
+		<li><b>Save & Retrieve:</b> Save JSON with a title for future reference.</li>
+		<li><b>Schema Validation:</b> Validate JSON against schemas and save schemas for reuse.</li>
+		<li><b>JSON Path Evaluation:</b> Query the JSON tree using JSON Path expressions.</li>
 	</ul>
 	<a class="btn btn-lg btn-purple" href="/app">GET STARTED</a>
 </div>
 
 <style>
 	.page-center {
-		margin-left: 20%;
-		margin-top: 10%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+	ul {
+		text-align: left;
 	}
 	h1 {
 		font-family: 'Arial Black';
